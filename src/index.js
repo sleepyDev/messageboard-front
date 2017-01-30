a160f94fdf7c84eecdad72b4f2b336a643548397
Topi - <Topi ->
2017-01-30 11:20:31 +0200

    removed sse support

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ import store from "./store"
 
 const app = document.getElementById("root")
 
-var es = new EventSource("http://localhost:4399/sse");
-es.onmessage = (event) => {
-    console.log(event.data)
-}
-
 ReactDOM.render(
 <Provider store={store}>
     <Layout/>
